0478d873499dc2236f006ef43b3dad05ca7dc1c6
affulti <affulti@outlook.com>
2024-07-23 20:44:23 +0800

    modify test

--- tests/JettonInstance.spec.ts
+++ tests/JettonInstance.spec.ts
@@ -1,52 +1,25 @@
-import {
-    Blockchain, 
-    SandboxContract, 
-    TreasuryContract,
-    printTransactionFees,
-    prettyLogTransactions,
-    RemoteBlockchainStorage,
-    wrapTonClient4ForRemote,
-    } from "@ton/sandbox";
-import { Address, beginCell, fromNano, StateInit, toNano } from "@ton/core";
-import { TonClient4 } from "@ton/ton";
-import "@ton/test-utils";
+import { Blockchain, SandboxContract, TreasuryContract } from "@ton/sandbox";
+import { Address, beginCell, toNano } from "@ton/core";
 import { buildOnchainMetadata } from "../utils/jetton-helpers";
-import { printSeparator } from "../utils/print";
+import "@ton/test-utils";
 
 //==== Contract SDK ====//
-import { SampleJetton, Mint, TokenTransfer } from "../wrappers/SampleJetton";
-import { JettonWallet, TokenBurn } from "../wrappers/JettonWallet";
+import { JettonInstance, Mint, Transfer } from "../wrappers/JettonInstance";
+import { JettonWallet, Burn } from "../wrappers/JettonWallet";
 
-//==== DeDust.io SDK ====//
-import {
-    Asset,
-    Factory,
-    MAINNET_FACTORY_ADDR,
-    PoolType,
-    Vault,
-    LiquidityDeposit,
-    VaultJetton,
-    JettonRoot,
-    ReadinessStatus,
-} from "@dedust/sdk";
-
-//==== STON.fi SDK ====//
-import TonWeb from "tonweb";
-import { Router, ROUTER_REVISION, ROUTER_REVISION_ADDRESS } from "@ston-fi/sdk";
-
-const sampleJettonParams = {
-    name: "Liber Token",
-    description: "This is description of Test tact sampleJetton",
-    symbol: "LT",
-    image: "https://play-lh.googleusercontent.com/ahJtMe0vfOlAu1XJVQ6rcaGrQBgtrEZQefHy7SXB7jpijKhu1Kkox90XDuH8RmcBOXNn",
+const jettonParams = {
+    name: "Test Jetton",
+    description: "This is description of Test Jetton",
+    symbol: "TJ",
+    image: "https://avatars.githubusercontent.com/u/104382459?s=200&v=4",
 };
-let content = buildOnchainMetadata(sampleJettonParams);
+let jettonContent = buildOnchainMetadata(jettonParams);
 let maxSupply = toNano(100000000000);
 
-describe("SampleJetton", () => {
+describe("JettonInstance", () => {
 
     let blockchain: Blockchain;
-    let sampleJetton: SandboxContract<SampleJetton>;
+    let jettonInstance: SandboxContract<JettonInstance>;
     let deployerWallet: SandboxContract<JettonWallet>;
     let deployer: SandboxContract<TreasuryContract>; 
 
@@ -55,34 +28,34 @@ describe("SampleJetton", () => {
         //Create content Cell
         blockchain = await Blockchain.create();
         deployer = await blockchain.treasury("deployer");
-        sampleJetton = blockchain.openContract(await SampleJetton.fromInit(deployer.address, content, maxSupply));
+        jettonInstance = blockchain.openContract(await JettonInstance.fromInit(deployer.address, jettonContent, maxSupply));
         
         //Send Transaction
-        const deployResult = await sampleJetton.send(deployer.getSender(), { value: toNano("10") }, "Mint: 100");
+        const deployResult = await jettonInstance.send(deployer.getSender(), { value: toNano("10") }, "Mint: 100");
         expect(deployResult.transactions).toHaveTransaction({
             from: deployer.address,
-            to: sampleJetton.address,
+            to: jettonInstance.address,
             deploy: true,
             success: true,
         });
 
-        const deployerWalletAddress = await sampleJetton.getGetWalletAddress(deployer.address);
+        const deployerWalletAddress = await jettonInstance.getGetWalletAddress(deployer.address);
         deployerWallet = blockchain.openContract(await JettonWallet.fromAddress(deployerWalletAddress));
     });
 
     it("Whether contract deployed successfully", async () => {
-        //the check is done inside beforeEach, blockchain and sampleJetton are ready to use
-        console.log((await sampleJetton.getGetJettonData()).totalSupply);
-        console.log((await sampleJetton.getGetJettonData()).mintable);
-        console.log((await sampleJetton.getGetJettonData()).owner);
-        console.log((await sampleJetton.getGetJettonData()).content)
+        //the check is done inside beforeEach, blockchain and jettonInstance are ready to use
+        console.log((await jettonInstance.getGetJettonData()).totalSupply);
+        console.log((await jettonInstance.getGetJettonData()).mintable);
+        console.log((await jettonInstance.getGetJettonData()).owner);
+        console.log((await jettonInstance.getGetJettonData()).jettonContent)
     });
 
     it("Convert Address Format", async () => {
-        console.log("Jetton Master Contract: " + sampleJetton.address);
-        console.log("Is Friendly Address: " + Address.isFriendly(sampleJetton.address.toString()));
+        console.log("Jetton Master Contract: " + jettonInstance.address);
+        console.log("Is Friendly Address: " + Address.isFriendly(jettonInstance.address.toString()));
 
-        const testAddr = Address.parse(sampleJetton.address.toString());
+        const testAddr = Address.parse(jettonInstance.address.toString());
         console.log("@ Address: ", + testAddr.toString( { bounceable: false }));
         console.log("@ Address: ", testAddr.toString());
         console.log("@ Address(urlSafe: true): " + testAddr.toString( { urlSafe: true }));
@@ -91,21 +64,21 @@ describe("SampleJetton", () => {
     });
 
     it("Should mint successfully", async () => {
-        const totalSupplyBefore = (await sampleJetton.getGetJettonData()).totalSupply;
+        const totalSupplyBefore = (await jettonInstance.getGetJettonData()).totalSupply;
         const mintAmount = toNano(100);
         const mintMessage: Mint = {
             $$type: "Mint", 
             amount: mintAmount,
             receiver: deployer.address,
         };
-        const mintResult =await  sampleJetton.send(deployer.getSender(),{ value: toNano("10") }, mintMessage);
+        const mintResult =await  jettonInstance.send(deployer.getSender(),{ value: toNano("10") }, mintMessage);
         expect(mintResult.transactions).toHaveTransaction({
             from: deployer.address,
-            to: sampleJetton.address,
+            to: jettonInstance.address,
             success: true,
         });
 
-        const totalSupplyAfter = (await sampleJetton.getGetJettonData()).totalSupply;
+        const totalSupplyAfter = (await jettonInstance.getGetJettonData()).totalSupply;
         expect(totalSupplyBefore + mintAmount).toEqual(totalSupplyAfter);
 
         const walletData = await deployerWallet.getGetWalletData();
@@ -124,30 +97,30 @@ describe("SampleJetton", () => {
             amount: mintAmount,
             receiver: sender.address,
         };
-        await sampleJetton.send(deployer.getSender(), { value: toNano("0.25") }, mintMessage);
+        await jettonInstance.send(deployer.getSender(), { value: toNano("0.25") }, mintMessage);
 
-        const senderWalletAddress = await sampleJetton.getGetWalletAddress(sender.address);
+        const senderWalletAddress = await jettonInstance.getGetWalletAddress(sender.address);
         const senderWallet = blockchain.openContract(JettonWallet.fromAddress(senderWalletAddress));
 
         //Transfer tokens from sender's wallet to receiver's wallet
-        const tokenTransferMessage: TokenTransfer = {
-            $$type: "TokenTransfer",
+        const transferMessage: Transfer = {
+            $$type: "Transfer",
             queryId: 0n,
             amount: transferAmount,
-            receiver: receiver.address,  //receiver.address ??
+            destination: receiver.address,  //receiver.address ??
             responseDestination: sender.address,
             customPayload: null,
             forwardTonAmount: toNano("0.1"),
             forwardPayload: beginCell().storeUint(0, 1).storeUint(0, 32).endCell(),
         };
-        const tokenTransferResult = await senderWallet.send(sender.getSender(), { value: toNano("0.5") }, tokenTransferMessage);
-        expect(tokenTransferResult.transactions).toHaveTransaction({
+        const transferResult = await senderWallet.send(sender.getSender(), { value: toNano("0.5") }, transferMessage);
+        expect(transferResult.transactions).toHaveTransaction({
             from: sender.address,
             to: senderWallet.address,  //why
             success: true,
         });
 
-        const receiverWalletAddress = await sampleJetton.getGetWalletAddress(receiver.address);
+        const receiverWalletAddress = await jettonInstance.getGetWalletAddress(receiver.address);
         const receiverWallet = blockchain.openContract(JettonWallet.fromAddress(receiverWalletAddress));
 
         const senderBalanceAter = (await senderWallet.getGetWalletData()).balance;
@@ -171,14 +144,14 @@ describe("SampleJetton", () => {
             amount: mintAmount,
             receiver: deployer.address,
         };
-        await sampleJetton.send(deployer.getSender(), { value: toNano(1) }, mintMessage);
+        await jettonInstance.send(deployer.getSender(), { value: toNano(1) }, mintMessage);
 
         let deployerBalanceBefore = (await deployerWallet.getGetWalletData()).balance;
         expect(deployerBalanceBefore).toEqual(deployerBalanceInit + mintAmount);
 
         let burnAmount = toNano(10);
-        const burnMessage: TokenBurn = {
-            $$type: "TokenBurn",
+        const burnMessage: Burn = {
+            $$type: "Burn",
             queryId: 0n,
             amount: burnAmount,
             responseDestination: deployer.address,
@@ -198,16 +171,16 @@ describe("SampleJetton", () => {
             amount: mintAmount,
             receiver: player.address,
         };
-        await sampleJetton.send(deployer.getSender(), { value: toNano(1) }, mintMessage);
+        await jettonInstance.send(deployer.getSender(), { value: toNano(1) }, mintMessage);
 
-        let totalSupplyBefore = (await sampleJetton.getGetJettonData()).totalSupply;
-        const mintResult = await sampleJetton.send(player.getSender(), { value: toNano(1) }, mintMessage);
+        let totalSupplyBefore = (await jettonInstance.getGetJettonData()).totalSupply;
+        const mintResult = await jettonInstance.send(player.getSender(), { value: toNano(1) }, mintMessage);
         expect(mintResult.transactions).toHaveTransaction({
             from: player.address,
-            to: sampleJetton.address,
+            to: jettonInstance.address,
         });
 
-        let totalSupplyAfter = (await sampleJetton.getGetJettonData()).totalSupply;
+        let totalSupplyAfter = (await jettonInstance.getGetJettonData()).totalSupply;
         expect(totalSupplyAfter).toEqual(totalSupplyBefore);
     });
 })
